f26cfe73c2d5892d94c97570bc4a1ea73614fe88
Yong Bakos <ybakos@humanoriented.com>
2018-03-23 16:00:11 -0700

    Test 'create' link.

--- test/features/user-visits-root-test.js
+++ test/features/user-visits-root-test.js
@@ -7,4 +7,11 @@ describe('User visits root', () => {
       assert.equal(browser.getText('#items-container'), '');
     });
   });
+  describe('and can navigate', () => {
+    it('to /create.html', () => {
+      browser.url('/');
+      browser.click('a[href="create.html"]');
+      assert.include(browser.getText('body'), 'Create');
+    });
+  });
 });
