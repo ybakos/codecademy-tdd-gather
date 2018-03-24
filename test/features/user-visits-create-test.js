6d0a61b778650f15ee19f2f52bc277f5cd6e69ec
Yong Bakos <ybakos@humanoriented.com>
2018-03-24 08:02:58 -0700

    Test item creation front-end.

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -1,5 +1,18 @@
 const {assert} = require('chai');
 const {buildItemObject} = require('../test-utils');
 
-// Add your tests below:
+describe('User visits the create page', () => {
+  describe('posts an item', () => {
+    it('sees it rendered', () => {
+      const item = buildItemObject();
 
+      browser.url('/create.html');
+      browser.setValue('#title-input', item.title);
+      browser.setValue('#description-input', item.description);
+      browser.setValue('#imageUrl-input', item.imageUrl);
+      browser.click('#submit-button');
+      assert.include(browser.getText('body'), item.title);
+      assert.include(browser.getAttribute('body img', 'src'), item.imageUrl);
+    });
+  });
+});
