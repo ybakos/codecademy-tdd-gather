d08c371ff71292c1aea7544d252897f3b27d802b
Yong Bakos <ybakos@humanoriented.com>
2018-04-07 16:55:05 -0700

    Phase 4, step 2: Correctly seed fake item for feature test.

--- test/features/user-visits-single-item-test.js
+++ test/features/user-visits-single-item-test.js
@@ -3,18 +3,20 @@ const {seedItemToDatabase} = require('../test-utils');
 
 describe('User visits a single item page', () => {
 
-  it('sees item details', async () => {
-    const title = "Fake Item";
-    const description = "Fake description";
-    const imageUrl = "http://fake.com";
-    const fakeItem = {title, description, imageUrl};
+  const title = "Fake Item";
+  const description = "Fake description";
+  const imageUrl = "http://fake.com";
+  const fakeItem = {title, description, imageUrl};
 
-    await seedItemToDatabase(fakeItem);
+  before(async function() {
+    seedItemToDatabase(fakeItem);
+  });
 
+  it('sees item details', () => {
     browser.url('/');
     browser.click('.item-card a');
-    assert.include(browser.getText('body'), fakeItem.title);
-    assert.include(browser.getText('body'), fakeItem.description);
-    assert.include(browser.getAttribute('body img', 'src'), fakeItem.imageUrl);
+    assert.include(browser.getText('#item-title'), fakeItem.title);
+    assert.include(browser.getText('#item-description'), fakeItem.description);
+    assert.include(browser.getAttribute('.single-item-img img', 'src'), fakeItem.imageUrl);
   });
 });
