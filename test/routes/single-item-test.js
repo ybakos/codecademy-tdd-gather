381c3985c96f46a4ddec68a8f4b300999ac60292
Yong Bakos <ybakos@humanoriented.com>
2018-04-07 17:33:20 -0700

    Refactor test within GET describe function.

--- test/routes/single-item-test.js
+++ test/routes/single-item-test.js
@@ -11,18 +11,20 @@ describe('Server path: /items/:id', () => {
 
   afterEach(disconnectDatabase);
 
-  it('includes the item attributes in the response body', async () => {
-    const title = "Fake Item";
-    const description = "Fake description";
-    const imageUrl = "http://fake.com";
-    const itemAttributes = {title, description, imageUrl};
+  describe('GET', () => {
+    it('includes the item attributes in the response body', async () => {
+      const title = "Fake Item";
+      const description = "Fake description";
+      const imageUrl = "http://fake.com";
+      const itemAttributes = {title, description, imageUrl};
 
-    const createdItem = await seedItemToDatabase(itemAttributes);
+      const createdItem = await seedItemToDatabase(itemAttributes);
 
-    const response = await request(app).get(`/items/${createdItem._id}`);
-    assert.include(parseTextFromHTML(response.text, '#item-title'), createdItem.title);
-    assert.include(parseTextFromHTML(response.text, '#item-description'), createdItem.description);
+      const response = await request(app).get(`/items/${createdItem._id}`);
+      assert.include(parseTextFromHTML(response.text, '#item-title'), createdItem.title);
+      assert.include(parseTextFromHTML(response.text, '#item-description'), createdItem.description);
 
+    });
   });
 
 });
